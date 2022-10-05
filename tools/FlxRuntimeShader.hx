5884cd137ffb26afca81e6ab071c0ce4e05323bf
jonnyboy <107599365+ItsyourboyJonnycat@users.noreply.github.com>
2022-10-05 09:44:05 -0700

    added sampler2D https://github.com/ShadowMario/FNF-PsychEngine/pull/10649

--- tools/FlxRuntimeShader.hx
+++ tools/FlxRuntimeShader.hx
@@ -763,6 +763,22 @@ class FlxRuntimeShader extends FlxShader
 			return null;
 		}
 		return prop.value;
+	}  
+	
+	/**
+	 * Set or modify a sampler2D input of the shader.
+	 * @param name The name of the shader input to modify.
+	 * @param value The texture to use as the sampler2D input.
+	 */
+	public function setSampler2D(name:String, value:BitmapData)
+	{
+		var prop:ShaderInput<BitmapData> = Reflect.field(this.data, name);
+		if(prop == null)
+		{
+			trace('[WARNING] Shader sampler2D property ${name} not found.');
+			return;
+		}
+		prop.input = value;
 	}
 
 	/**
